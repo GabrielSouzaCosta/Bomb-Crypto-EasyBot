72a889462593c4402bfc9d573ce75f9a62e0ab8d
Gabriel Souza Costa <gabrielsscosta2010@hotmail.com>
2021-12-01 21:02:51 -0300

    randomize

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from PIL.ImageOps import grayscale
 import pyautogui
 from time import sleep
+import random
+
+rg = random.uniform
+small_time = [2.1, 3.9]
+medium_time = [4.9, 7.2]
+large_time = [8.1, 11.2]
+larger_time = [24.3, 30.4]
+big_time = [380.6, 423.7]
+
 
 
 class Bot():
     def __init__(self):
+        pyautogui.PAUSE = random.uniform(1.1, 1.4)
         self.x, self.y = pyautogui.size()
         print('Bot waking up...')
         print('ctrl + c to kill Bot')
         self.main_chain()
     
     def main_chain(self):
-        sleep(3)
+        sleep(rg(*medium_time))
         while True:
             self.login()
             heroes_work = self.put_heroes_to_work()
@@ -23,22 +33,22 @@ class Bot():
 
     def login(self):
         print("Trying to log in...")
-        sleep(3)
+        sleep(rg(*small_time))
         self.refresh_page()
-        sleep(10)
+        sleep(rg(*large_time))
         wallet_check = self.check('images/wallet.png')
         pyautogui.click(wallet_check) if wallet_check else print('Wallet button not found')
         # sleep(2)
         # fox_check = self.check('images/metamask.png')
         # pyautogui.click(fox_check) if fox_check else print('Metamask fox not found')
-        sleep(8)
+        sleep(rg(*small_time))
         sign_check = self.check('images/sign.png', confidence=0.6)
         pyautogui.click(sign_check, clicks=3, interval=0.5) if sign_check else print('Sign button not found')
-        sleep(30)
+        sleep(rg(*larger_time))
 
     def put_heroes_to_work(self):
         print("Putting your slaves to work")
-        sleep(3)
+        sleep(rg(*small_time))
         heroes_check = self.check('images/heroes.png')
         if heroes_check: 
             pyautogui.click(heroes_check) 
@@ -46,13 +56,13 @@ class Bot():
             print("Maybe your not in the main screen, trying to enter again")
             return False
 
-        sleep(10)
+        sleep(rg(*large_time))
         scroll_x, scroll_y = pyautogui.locateCenterOnScreen('images\scroll.png')
         for t in range(2):
             pyautogui.click(scroll_x, scroll_y)
             pyautogui.dragTo(scroll_x, 200, duration=1.5)   
         self.click_work_button()
-        sleep(1.2)
+        sleep(rg(*small_time))
         x_button_check = self.check('images/closeHeroes.png')
         pyautogui.click(x_button_check, clicks=2, interval=1) if x_button_check else print('Close button image not found')
         return True
@@ -71,11 +81,12 @@ class Bot():
         treasure_check = self.check('images/treasureScreen.png')
         pyautogui.click(treasure_check, clicks=2, interval=1) if treasure_check else print('Treasure hunt not found')
         while True:
-            sleep(120)
+            refresher = random.randint(4, 7)
+            sleep(rg(*big_time))
             pyautogui.click()
             self.refresh_treasure()
             refresh_count += 1
-            if refresh_count >= 10 or self.not_found_count >= 2:
+            if refresh_count >= refresher or self.not_found_count >= 2:
                 self.refresh_page()
                 break
 
@@ -88,7 +99,7 @@ class Bot():
         else:
             print('Back icon not found')
             self.not_found_count += 1
-        sleep(1.5)
+        sleep(rg(*small_time))
         treasure_check = self.check('images/treasureScreen.png')
         pyautogui.click(treasure_check, clicks=2, interval=1) if treasure_check else print('Treasure hunt not found')
 
@@ -102,7 +113,7 @@ class Bot():
 
 def main():
     pyautogui.FAILSAFE = False
-    pyautogui.PAUSE = 1.2
+    pyautogui.PAUSE = random.uniform(1.1, 1.4)
     bot = Bot()
 
 
---
+++ test.py
@@ -0,0 +1,10 @@
+import random
+
+small_time = [2.1, 3.9]
+rand_generator = random.uniform
+
+print(rand_generator(*small_time))
+
+
+print(random.uniform(*small_time))
+
